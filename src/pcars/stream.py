ad51ae04aa1a7b84a3e350dc7c86ae3aa0d432c8
James Muscat <jamesremuscat@gmail.com>
2016-01-06 19:14:58 +0000

    Add a simple multicast packet receiver.

---
+++ src/pcars/stream.py
@@ -0,0 +1,34 @@
+from pcars.packet import Packet
+from threading import Thread
+import socket
+import struct
+
+
+MCAST_ANY = "224.0.0.1"
+
+
+class PCarsStreamReceiver(Thread):
+
+    def __init__(self, port=5606):
+        super(PCarsStreamReceiver, self).__init__()
+        self.port = port
+        self.setDaemon(True)
+        self.listeners = []
+
+    def addListener(self, listener):
+        self.listeners.append(listener)
+
+    def run(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        # Bind to the server address
+        sock.bind(('', self.port))
+        group = socket.inet_aton(MCAST_ANY)
+        mreq = struct.pack("4sL", group, socket.INADDR_ANY)
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+        while True:
+            data = sock.recv()
+            packet = Packet.readFrom(data)
+            for listener in self.listeners:
+                listener.handlePacket(packet)
