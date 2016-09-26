fe6375888144b194ed78535d142ad98f619c8e4f
Andrew Fyfe <andrew@neptune-one.net>
2016-09-26 14:48:22 +0100

    Test case for ParticipantInfoStringsAdditionalPacket.

---
+++ src/pcars/test/test_participant_info_strings_additional_packet.py
@@ -0,0 +1,27 @@
+from pcars.packet import Packet, ParticipantInfoStringsAdditionalPacket
+from unittest import TestCase
+import os
+
+
+_location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+
+class TestParticipantInfoStringsAdditionalPacket(TestCase):
+
+    def testParseSamplePacket(self):
+        f = open(os.path.join(_location, "ParticipantInfoStringsAdditionalPacket_1.bin"), "rb")
+        p = Packet.readFrom(f)
+        self.assertEqual(ParticipantInfoStringsAdditionalPacket, p.__class__)
+
+        # Packet Header
+        self.assertEqual(1235, p.buildVersion)
+        self.assertEqual(2, p.packetType)
+        self.assertEqual(32, p.sequenceNumber)
+
+        # Packet Data
+        self.assertEqual(32, p["offset"])
+
+        # Participants
+        self.assertEqual(16, len(p["participants"]))
+        self.assertEqual("Abdullah El Khereiji", p["participants"][0]["name"])
+        self.assertEqual("Dominic Sch\\xf6nberner", p["participants"][15]["name"])
